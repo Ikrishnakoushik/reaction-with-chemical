fb9caea4a5c176def16b1099e6bcb94481c08223
Ikrishan <ikowshikreddy800@gmail.com>
2026-02-03 19:08:42 +0530

    Fix port for Render

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname));
 
 // Signup page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'signup.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Reaction page
