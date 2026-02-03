8becde23ce7665ff5f2a3b070f0cc9f44b4ba56b
Ikrishan <ikowshikreddy800@gmail.com>
2026-02-03 14:12:32 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(__dirname));
+
+// Signup page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'signup.html'));
+});
+
+// Reaction page
+app.get('/reaction', (req, res) => {
+  res.sendFile(path.join(__dirname, 'reaction.html'));
+});
+
+// Signup API
+app.post('/signup', (req, res) => {
+  const { name, email, password } = req.body;
+
+  console.log("New user:", name, email);
+
+  // (later we’ll save to database)
+  res.json({ success: true });
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
